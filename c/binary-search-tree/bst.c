357a54e935c7e3da225e3b279d47bffaf1bdbf5c
fernandocostar <fernandocr@id.uff.br>
2017-12-19 01:45:30 -0200

    Search function finished.

--- c/binary-search-tree/bst.c
+++ c/binary-search-tree/bst.c
@@ -1,30 +1,28 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct no{
-	int data;
-	struct no* esq;
-	struct no* dir; 
-}No;
-
-typedef No* Tree;
+#include "bst.h"
 
 No* new_node(int d){
 	No* noh = (No*)malloc(sizeof(No));
-	noh->dir = NULL;
-	noh->esq = NULL;
+	noh->right = NULL;
+	noh->left = NULL;
 	noh->data = d;
 	return noh;
 }
 
 void free_tree(Tree t){
 	if(t){
-		No* dir = t->dir;
-		No* esq = t->esq;
+		No* right = t->right;
+		No* left = t->left;
 		free(t);
-		free_tree(t->dir);
-		free_tree(t->esq);
+		free_tree(t->right);
+		free_tree(t->left);
 	}
+}
+
+No* search(Tree a, int t){
+	if(!a) return NULL;
+	if(a->data == t) return a;
+	if(a->data > t) return search(a->left, t);
+	return search(a->right, t);
 }
 
 void insert(Tree *a, int data){
@@ -33,67 +31,32 @@ void insert(Tree *a, int data){
 		return;
 	}
 	if(data > (*a)->data){
-		insert(&((*a)->dir), data);
+		insert(&((*a)->right), data);
 		return;
 	}
-	insert(&((*a)->esq), data);
+	insert(&((*a)->left), data);
 }
-
-/*
-No* insert(No* a, int data){
-	if (!a) return new_node(data);
-	if (a->data < data) return insert(a->dir,data);
-	return insert(a->esq,data);
-}
-*/
 
 void pre_ordem(Tree a){
 	if(a){
 		printf(" %d", a->data);
-		pre_ordem(a->esq);
-		pre_ordem(a->dir);
+		pre_ordem(a->left);
+		pre_ordem(a->right);
 	} 
 }
 
 void in_ordem(Tree a){
 	if(a){
-		in_ordem(a->esq);
+		in_ordem(a->left);
 		printf(" %d", a->data);
-		in_ordem(a->dir);
+		in_ordem(a->right);
 	} 
 }
 
 void pos_ordem(Tree a){
 	if(a){
-		pos_ordem(a->esq);
-		pos_ordem(a->dir);
+		pos_ordem(a->left);
+		pos_ordem(a->right);
 		printf(" %d", a->data);
 	} 
-}
-
-int main(){
-	int i, n;
-	scanf("%d", &n);
-	for(i = 0; i < n; i++){
-		printf("Case %d:\n", i+1);
-		int k, j, aux;
-		scanf("%d", &k);
-		Tree l = NULL;
-		for(j = 0; j < k; j++){
-			scanf("%d", &aux);
-			insert(&l, aux);
-		}
-		printf("Pre.:");
-		pre_ordem(l);
-		printf("\n");
-		printf("In..:");
-		in_ordem(l);
-		printf("\n");
-		printf("Post:");
-		pos_ordem(l);
-		printf("\n");
-		free_tree(l);
-		printf("\n");
-	}
-	return 0;
 }
